6f4681c345fb012c389fa24e48c6c01054c8cbc8
jaymalhar <krishnasudke00@gmail.com>
2025-09-21 21:27:15 +0530

    Add files via upload

---
+++ 1st program.py
@@ -0,0 +1,39 @@
+# print("hello world")
+# '''This is my 1st code'''
+# print("Learn More md Saddam") 
+
+# String
+# "Hello World"
+# 'Hello World'
+# '''Hello World'''
+# """Hello World"""
+
+# Variables & datatypes
+# {int}--proper values,{float}--decimal points values,{bool}--true or false
+                                    # {str}--anything name  {complex}-- equation
+
+
+# int-5,9,4,2
+# float-1.0,2.0,3.5
+# bool- True,False
+# str-"Krushna"
+# complex- 4-5j
+
+# Variables
+
+# name="Saddam"
+# age=20
+# print(name)
+# print(age)
+
+# Operators
+# a=5
+# b=2
+# print(a/b) # Full Quot
+# print(a//b) # Int Quot
+# print(a%b) # Remaindor
+
+a = "5"
+b = "4"
+c= a+b
+print(c)
